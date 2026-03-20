f02d4f4da60b16a036902484057d54a19ebaff0d
William Berg Araújo Diogo <82834801+williamberg123@users.noreply.github.com>
2026-03-20 01:39:24 +0000

    Finalizando o desafio nível mestre

--- xadrez.c
+++ xadrez.c
@@ -1,28 +1,57 @@
 #include <stdio.h>
 
-int main() {
-  int movimentoDaTorre = 1;
-  int movimentoDoBispo = 1;
-
-  printf("*** Movimentando a torre ***\n");
-
-  while (movimentoDaTorre <= 5){
-    printf("Direita\n");
-    movimentoDaTorre++;
-  }
-
-  printf("*** Movimentando o bispo ***\n");
-
-  do {
+void recursivoMovimentoDaTorre(int posicaoDaTorre, int numeroDeCasas) {
+  if (posicaoDaTorre <= numeroDeCasas) {
     printf("Cima, Direita\n");
-    movimentoDoBispo++;
-  } while (movimentoDoBispo <= 5);
-
-  printf("*** Movimentando a rainha ***\n");
-
-  for (int i = 1; i <=8; i++) {
-    printf("Esquerda\n");
+    recursivoMovimentoDaTorre(posicaoDaTorre + 1, numeroDeCasas);
   }
+}
+
+void recursivoMovimentoDoBispo(int posicaoDoBispo, int numeroDeCasas) {
+  if (posicaoDoBispo <= numeroDeCasas) {
+    printf("Cima, Direita\n");
+    recursivoMovimentoDoBispo(posicaoDoBispo + 1, numeroDeCasas);
+  }
+}
+
+void recursivoMovimentoDaRainha(int posicaoDaRainha, int numeroDeCasas) {
+  if (posicaoDaRainha <= numeroDeCasas) {
+    printf("Esquerda\n");
+    recursivoMovimentoDaRainha(posicaoDaRainha + 1, numeroDeCasas);
+  }
+}
+
+int main()
+{
+  // printf("*** Movimentando o cavalo ***\n");
+
+  // for (posicaoHorizontalDoCavalo; posicaoHorizontalDoCavalo <= numeroDeCasasParaEsquerdaDoCavalo; posicaoHorizontalDoCavalo++) {
+  //   while (posicaoVerticalDoCavalo <= numeroDeCasasParaBaixoDoCavalo) {
+  //     printf("Baixo\n");
+
+  //     posicaoVerticalDoCavalo++;
+  //   }
+
+  //   printf("Esquerda\n");
+  // }
+
+  int posicaoTorre = 1;
+  int numeroDeCasasTorre = 5;
+
+  printf("*** Movimento da Torre ***\n");
+  recursivoMovimentoDaTorre(posicaoTorre, numeroDeCasasTorre);
+
+  int posicaoBispo = 1;
+  int numeroDeCasasBispo = 5;
+
+  printf("*** Movimento do Bispo ***\n");
+  recursivoMovimentoDoBispo(posicaoBispo, numeroDeCasasBispo);
+
+  int posicaoRainha = 1;
+  int numeroDeCasasRainha = 8;
+
+  printf("*** Movimento da Rainha ***\n");
+  recursivoMovimentoDaRainha(posicaoRainha, numeroDeCasasRainha);
 
   return 0;
 }
